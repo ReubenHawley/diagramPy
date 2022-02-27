cbddab4df1e9a638e37d24d6a50f8d85ae3432c0
ReubenHawley <reubenhawley@protonmail.com>
2022-02-27 14:09:06 +0100

    create globals
    - create global primitive values and assign datatypes
    - create directory method in abstract class for global inheritance
    - refactor capitalize_first method to abstract class

--- Generator/IGenerator.py
+++ Generator/IGenerator.py
@@ -1,3 +1,4 @@
+import os
 from abc import ABC, abstractmethod
 
 
@@ -5,6 +6,24 @@ class IGenerator(ABC):
     """"
     Abstract class to form the contract for the implementing classes
     """
+    _diagram_type: str
+    _primitive_string: str
+    _primitive_int: str
+    _primitive_double: str
+    _primitive_float: str
+    _primitive_array: str
+    _primitive_object: str
+    _primitive_void: str
+    _private_prefix: str
+    _protected_prefix: str
+    _public_prefix: str
+
+    @staticmethod
+    def _generate_directory(path: str = None) -> None:
+        try:
+            os.mkdir(path)
+        except FileNotFoundError():
+            print("specified path does not exist")
 
     @abstractmethod
     def generate_document(self, path: str, generated_code: str):
@@ -33,3 +52,10 @@ class IGenerator(ABC):
         Create new class
         """
         pass
+
+    @staticmethod
+    def _capitalize_first(input_value: str) -> str:
+        return_value = input_value[0].upper()
+        return_value += input_value[1:].lower()
+        return return_value
+
