3b9341d4de3d55863574b9c35eaaf1e5ddf6763e
ReubenHawley <reubenhawley@protonmail.com>
2022-02-27 16:05:33 +0100

    expand abstract class
    - create global empties
    - move init to abstract class
    - create generate_document method
      - testing for path
      - generate directory if non exists
    - refactor parse_datatype to abstract class
    - create new parse_returntype method for handling return types in java and c#

--- Generator/IGenerator.py
+++ Generator/IGenerator.py
@@ -14,9 +14,20 @@ class IGenerator(ABC):
     _primitive_array: str
     _primitive_object: str
     _primitive_void: str
+    _primitive_boolean: str
+    _empty_string: str = '\"\"'
+    _empty_int: int = 0
+    _empty_float: float = 0
+    _empty_double: float = 0
     _private_prefix: str
     _protected_prefix: str
     _public_prefix: str
+    _extension: str
+
+    def __init__(self, diagram_type: str, elements: dict) -> None:
+        self.elements = elements
+        self._diagram_type = diagram_type
+        self.class_name = self._capitalize_first(self.elements['class'])
 
     @staticmethod
     def _generate_directory(path: str = None) -> None:
@@ -25,12 +36,19 @@ class IGenerator(ABC):
         except FileNotFoundError():
             print("specified path does not exist")
 
-    @abstractmethod
-    def generate_document(self, path: str, generated_code: str):
+    def generate_document(self, generated_code: str, path: str = None) -> None:
         """"
         Create the documents
         """
-        pass
+        if path is None:
+            path = os.path.join(os.getcwd(), f"{self._diagram_type}_diagram")
+        if os.path.exists(path):
+            with open(f'{path}/{self.class_name}{self._extension}', 'w') as file:
+                file.write(generated_code)
+        else:
+            self._generate_directory(path)
+            with open(f'{path}/{self.class_name}{self._extension}', 'w') as file:
+                file.write(generated_code)
 
     @abstractmethod
     def generate_class(self):
@@ -58,4 +76,30 @@ class IGenerator(ABC):
         return_value = input_value[0].upper()
         return_value += input_value[1:].lower()
         return return_value
+
+    def _parse_datatype(self, input_type: str) -> str:
+        if input_type.__eq__("string"):
+            return self._primitive_string
+        elif input_type.__eq__("int"):
+            return self._primitive_int
+        elif input_type.__eq__("double"):
+            return self._primitive_double
+        elif input_type.__eq__("float"):
+            return self._primitive_float
+        elif input_type.__eq__("void"):
+            return self._primitive_void
+        if input_type.__eq__("boolean"):
+            return self._primitive_boolean
+
+    def _parse_returntype(self, returntype: str):
+        if returntype.__eq__("int"):
+            return self._empty_int
+        elif returntype.__eq__("float"):
+            return self._empty_float
+        elif returntype.__eq__("boolean"):
+            return self._empty_boolean
+        elif returntype.__eq__("string"):
+            return self._empty_string
+        elif returntype.__eq__("double"):
+            return self._empty_double
 
