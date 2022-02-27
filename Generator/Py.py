a2aa8ced7b9b84d4f995f55a9d6c2df7c224386f
ReubenHawley <reubenhawley@protonmail.com>
2022-02-27 08:25:25 +0100

    create construct method in Constructor
    after defining the language input, the input language will be used to complete the construction of the string to write to file

--- Generator/Py.py
+++ Generator/Py.py
@@ -5,12 +5,18 @@ class Py(IGenerator):
     """"
     Generator for the python code, parent of IGenerator and child of Constructor Class
     """
+    elements: dict
+    diagram_type: str
+
+    def __init__(self, diagram_type: str, elements: dict) -> None:
+        self.elements = elements
+        self.diagram_type = diagram_type
 
     def generate_document(self):
         """"
         Create the documents
         """
-        pass
+        open(f'generatedDoc/{self.diagram_type}', 'w')
 
     def generate_class(self):
         """"
